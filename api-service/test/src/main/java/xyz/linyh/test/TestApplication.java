a85afcb3a9282f1571ef34dc1d5efb558b65bab2
lin <1583427850@qq.com>
2024-01-11 18:31:55 +0800

    fix:

--- api-service/test/src/main/java/xyz/linyh/test/TestApplication.java
+++ api-service/test/src/main/java/xyz/linyh/test/TestApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TestApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TestApplication.class,args);
+        SpringApplication.run(TestApplication.class, args);
     }
 }
