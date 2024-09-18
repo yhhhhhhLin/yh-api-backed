03579bd675cb50280df569c5ae52e91baae49a44
linzz <1583427850@qq.com>
2024-09-18 23:48:06 +0800

    feat: 增加helper

--- api-service/api-database/src/main/java/shop/linyh/controller/DscInterfaceController.java
+++ api-service/api-database/src/main/java/shop/linyh/controller/DscInterfaceController.java
@@ -2,12 +2,15 @@ package shop.linyh.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shop.linyh.helper.DscInterfaceHelper;
 import xyz.linyh.ducommon.common.BaseResponse;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class DscInterfaceController {
+
+    private DscInterfaceHelper dscInterfaceHelper;
 
     /**
      * 处理数据源接口
@@ -16,7 +19,7 @@ public class DscInterfaceController {
      */
     @PostMapping("/yhapiBatabase")
     public BaseResponse processDatasourceInterface(HttpServletRequest request){
-//        TODO 根据获取的用户id和接口地址查询获取接口，然后获取查询的数据库地址和查询的字段拼接sql
+        dscInterfaceHelper.queryData(request);
 
         return null;
     }
---
+++ api-service/api-database/src/main/java/shop/linyh/helper/DscInterfaceHelper.java
@@ -0,0 +1,25 @@
+package shop.linyh.helper;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author linzz
+ */
+@Component
+public class DscInterfaceHelper {
+
+    /**
+     * 拼接和执行sql，返回查询结果
+     * @param request
+     * @return
+     */
+    public Object queryData(HttpServletRequest request) {
+
+//        TODO 根据获取的用户id和接口地址查询获取接口，然后获取查询的数据库地址和查询的字段拼接sql
+
+//        TODO 返回格式
+        return null;
+    }
+}
