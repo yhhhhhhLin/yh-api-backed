50cacb2f570a3dafc8094af428e66f58106cbce3
linzz <1583427850@qq.com>
2024-09-20 23:12:42 +0800

    docs:

--- api-service/api-database/src/main/java/shop/linyh/helper/DscInterfaceHelper.java
+++ api-service/api-database/src/main/java/shop/linyh/helper/DscInterfaceHelper.java
@@ -18,6 +18,7 @@ public class DscInterfaceHelper {
     public Object queryData(HttpServletRequest request) {
 
 //        TODO 根据获取的用户id和接口地址查询获取接口，然后获取查询的数据库地址和查询的字段拼接sql
+        String accessKey = request.getHeader("accessKey");
 
 //        TODO 返回格式
         return null;
