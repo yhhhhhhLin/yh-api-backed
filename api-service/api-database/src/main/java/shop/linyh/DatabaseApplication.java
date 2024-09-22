fa11382405a7800bff22b89e9420fe5f1c14c3eb
linzz <1583427850@qq.com>
2024-09-22 22:07:36 +0800

    feat: 增加获取用户id

--- api-service/api-database/src/main/java/shop/linyh/DatabaseApplication.java
+++ api-service/api-database/src/main/java/shop/linyh/DatabaseApplication.java
@@ -1,10 +1,12 @@
 package shop.linyh;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author linzz
  */
 @SpringBootApplication
+@EnableDubbo
 public class DatabaseApplication {
 }
--- api-service/api-database/src/main/java/shop/linyh/helper/DscInterfaceHelper.java
+++ api-service/api-database/src/main/java/shop/linyh/helper/DscInterfaceHelper.java
@@ -1,6 +1,9 @@
 package shop.linyh.helper;
 
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Component;
+import xyz.linyh.dubboapi.service.DubboUserService;
+import xyz.linyh.model.user.entitys.User;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,6 +12,10 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Component
 public class DscInterfaceHelper {
+
+
+    @DubboReference
+    private DubboUserService dubboUserService;
 
     /**
      * 拼接和执行sql，返回查询结果
@@ -19,6 +26,14 @@ public class DscInterfaceHelper {
 
 //        TODO 根据获取的用户id和接口地址查询获取接口，然后获取查询的数据库地址和查询的字段拼接sql
         String accessKey = request.getHeader("accessKey");
+        User user = dubboUserService.getUserByAk(accessKey);
+        if(user == null) {
+//            TODO
+            System.out.println("错误");
+            return null;
+        }
+        Long userId = user.getId();
+
 
 //        TODO 返回格式
         return null;
